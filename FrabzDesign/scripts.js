31853093f8488ebf5e643d3737098660a3ebcb67
Ant818 <anthony.rogers1988@gmail.com>
2020-05-12 17:18:26 +0100

    Animation added on load of site.

--- FrabzDesign/scripts.js
+++ FrabzDesign/scripts.js
@@ -8,9 +8,37 @@ $(document).ready(function () {
   $("#text").html(text);
 
   /* Animation*/
-  TweenMax.to("#logo", 2, {
+  TweenMax.from("#outerBlackRing", 1.2, {
     opacity: 0,
-    y: -60,
-    ease: Expo.easeInOut
-}); 
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#blueRing", 1.6, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#whiteRing", 3, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#purpleRing", 3.4, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#innerBlackRing", 3.8, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#logo", 4.2, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+  TweenMax.from("#text", 7, {
+    opacity: 0,
+    ease: Expo.easeIn,
+  });
+
+/*rotation of circles*/
+TweenMax.to( "#purpleRing" , 3, { rotation: 360, ease: Linear.easeNone, repeat: -1 } );
+
+
 });
